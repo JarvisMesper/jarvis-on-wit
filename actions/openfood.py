2fed55ca481d50b81e745f5bd7f699dd55481e26
nathanquinteiro <nathan.quinteiro@gmail.com>
2017-04-22 22:45:21 +0200

    Handle unknown product on Openfood API

--- actions/openfood.py
+++ actions/openfood.py
@@ -15,11 +15,19 @@ def getOpenFoodInfo(request):
 
     product = wit.first_entity_value(entities, 'product_id')
     if product:
-        res = RequestOpenFood.get_product(barcode=product)
-        res = ProductBuilder.clean_data(res)
-        context['info'] = res
+
+        try:
+            res = RequestOpenFood.get_product(barcode=product)
+            res = ProductBuilder.clean_data(res)
+            context['info'] = res
+            if context.get('missing_id') is not None:
+                del context['missing_id']
+        except:
+            context['info'] = "Can't find this product on the OpenFood database"
     else:
-        context['info'] = "Can't retrieve any info, sorry"
+        context['missing_id'] = True
+        if context.get('info') is not None:
+            del context['info']
 
 
     return context
